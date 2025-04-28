12cc53f3b2f46d6cbd7f2f1a4911ee2ae6db2c8f
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-04-28 17:27:37 -0600

    Initial commit

---
+++ src/main/java/com/farid/xadis/XadisApplication.java
@@ -0,0 +1,13 @@
+package com.farid.xadis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XadisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XadisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farid/xadis/XadisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farid.xadis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XadisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
