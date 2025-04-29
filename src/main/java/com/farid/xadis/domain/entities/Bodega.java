ce1007c026e3dce17fb9f8d3879a975c00a7ed6d
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-04-28 18:02:38 -0600

    feat: add entity

---
+++ src/main/java/com/farid/xadis/domain/entities/Bodega.java
@@ -0,0 +1,53 @@
+package com.farid.xadis.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "bodegas")
+public class Bodega {
+
+    @Id
+    private String codigo;
+
+    private String nombre;
+
+    @Column(length = 1)
+    private String activo;
+
+    private String documentopto;
+
+    private String codigopto;
+
+    @Column(length = 2)
+    private String pais;
+
+    private String departamento;
+
+    private String provincia;
+
+    private String distrito;
+
+    private int ubigeo_pto;
+
+    private String direccion_pto;
+
+    private String procesawms;
+
+    private LocalDate fecha_procesawms;
+
+    private String metro_2;
+
+}
