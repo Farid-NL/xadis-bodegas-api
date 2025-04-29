e0290144da366de1d53baf6003ecb8388f416178
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-04-28 19:46:52 -0600

    feat: add service layer
    Creates an intermediary between Presentation layer and Persistence layer

--- src/main/java/com/farid/xadis/controllers/BodegaController.java
+++ src/main/java/com/farid/xadis/controllers/BodegaController.java
@@ -2,6 +2,7 @@ package com.farid.xadis.controllers;
 
 import com.farid.xadis.domain.entities.BodegaEntity;
 import com.farid.xadis.repositories.BodegaRepository;
+import com.farid.xadis.services.BodegaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BodegaController {
-    private final BodegaRepository bodegaRepository;
+    private final BodegaService bodegaService;
 
-    public BodegaController(BodegaRepository bodegaRepository) {
-        this.bodegaRepository = bodegaRepository;
+    public BodegaController(BodegaService bodegaService) {
+        this.bodegaService = bodegaService;
     }
 
     @PostMapping("/bodega")
     public BodegaEntity createBodega(@RequestBody BodegaEntity bodegaEntity) {
-        return bodegaRepository.save(bodegaEntity);
+        return bodegaService.create(bodegaEntity);
     }
 }
---
+++ src/main/java/com/farid/xadis/services/BodegaService.java
@@ -0,0 +1,7 @@
+package com.farid.xadis.services;
+
+import com.farid.xadis.domain.entities.BodegaEntity;
+
+public interface BodegaService {
+    BodegaEntity create(BodegaEntity bodegaEntity);
+}
