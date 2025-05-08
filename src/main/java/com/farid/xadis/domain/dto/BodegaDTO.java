29607083afa153b425417aa3a7af363001f9dce4
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-05-08 07:28:05 -0600

    refactor: use validation groups instead of separate dto for update endpoints

--- src/main/java/com/farid/xadis/domain/dto/BodegaDTO.java
+++ src/main/java/com/farid/xadis/domain/dto/BodegaDTO.java
@@ -1,5 +1,7 @@
 package com.farid.xadis.domain.dto;
 
+import com.farid.xadis.validations.groups.OnCreate;
+import com.farid.xadis.validations.groups.OnUpdate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
@@ -22,58 +24,58 @@ public class BodegaDTO {
     private Long id;
 
     @Schema(example = "01")
-    @NotBlank(message = "'codigo' is necessary")
+    @NotBlank(groups = OnCreate.class)
     private String codigo;
 
     @Schema(example = "01 ALMACEN OBSOLETOS")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String nombre;
 
     @Schema(example = "A")
-    @NotBlank
-    @Size(min = 1, max = 1)
+    @NotBlank(groups = OnCreate.class)
+    @Size(groups = {OnCreate.class, OnUpdate.class}, min = 1, max = 1)
     private String activo;
 
     @Schema(example = "20427799973")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String documentopto;
 
     @Schema(example = "0101")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String codigopto;
 
     @Schema(example = "PE")
-    @NotBlank
-    @Size(min = 2, max = 2)
+    @NotBlank(groups = OnCreate.class)
+    @Size(groups = {OnCreate.class, OnUpdate.class}, min = 2, max = 2)
     private String pais;
 
     @Schema(example = "15")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String departamento;
 
     @Schema(example = "LIMA")
     private String provincia;
 
     @Schema(example = "ATE")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String distrito;
 
     @Schema(example = "150103")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String ubigeo_pto;
 
     @Schema(example = "CALLE VULCANO N° 176 LOTE N° 6 MZ. F URB. INDUSTRIAL")
-    @NotBlank
+    @NotBlank(groups = OnCreate.class)
     private String direccion_pto;
 
     @Schema(example = "S")
-    @NotBlank
-    @Size(min = 1, max = 1)
+    @NotBlank(groups = OnCreate.class)
+    @Size(groups = {OnCreate.class, OnUpdate.class}, min = 1, max = 1)
     private String procesawms;
 
     @Schema(example = "04/22/2025")
     @JsonFormat(pattern = "MM/dd/yyyy")
-    @NotNull
+    @NotNull(groups = OnCreate.class)
     private LocalDate fecha_procesawms;
 
     @Schema(example = "null")
