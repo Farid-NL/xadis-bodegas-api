c5644b59d4362891cd858ff4a695214f13e7e05a
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2025-05-14 15:56:34 -0600

    feat: add Flyway migrations

--- src/main/java/com/farid/xadis/repositories/BodegaRepository.java
+++ src/main/java/com/farid/xadis/repositories/BodegaRepository.java
@@ -2,6 +2,8 @@ package com.farid.xadis.repositories;
 
 import com.farid.xadis.domain.entities.BodegaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BodegaRepository extends JpaRepository<BodegaEntity, Long> {
 }
